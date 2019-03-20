1f3a2cc4ee16431553cc331755d108ff8f5101ee
Muhammad Haseeb <haseeb.lums@gmail.com>
2019-03-20 18:20:26 +0500

    Update background.js

--- HalfHulk/background.js
+++ HalfHulk/background.js
@@ -10,6 +10,7 @@ console.log("Doing...")
 sws_info		  = [] // sws are all software in this context, {extensions, apps, themes}
 ext_pages 		  = []
 headers_store     = {} //{requestId1:[http-headers1], requestId2:[http-headers2], ...}
+req_headers		  = {} //{requestId1:[http-headers1], requestId2:[http-headers2], ...}
 parameters_store  = {} //{requestId1:[url-parameters], requestId2:[url-parameters], ...}
 _alert  		  = "\n\nALERT\n"
 _hdr_rm 		  = "----------Headers Removed"
@@ -18,14 +19,20 @@ _hdr_ch			  = "!=!=!=!=!=!=!= Headers Changed"
 _hdr_hp			  = ":) :) :) :) :)HAPPY HAPPY Headers"
 _prm_ch			  = "!=!=!=!=!=!=!= Parameters Changed"
 _info   		  = "***********(i)"
-
+_avoid_page		  = "https://www.facebook.com/"
 
 
 
 // A utility function, takes two headers and checks for any differences
-let validateHeaders = function(stored_headers, new_headers){
+let validateHeaders = function(stored_headers, new_headers, t){ //t = 1 => response headers, t = 2 => request headers
+	
+
 	if (stored_headers.length != new_headers.length){
-		if (stored_headers.length > new_headers.length){console.log(_alert, _hdr_rm);}
+		if(t == 1){console.log(_info, "For response headers")}
+		else if (t == 2){console.log(_info, "For request headers")}
+		if (stored_headers.length > new_headers.length){
+			console.log(_alert, _hdr_rm);
+		}
 		else {console.log(_alert, _hdr_ad);}
 	}
 	else if(JSON.stringify(stored_headers) != JSON.stringify(new_headers)){//code performance alert
@@ -35,12 +42,14 @@ let validateHeaders = function(stored_headers, new_headers){
 			_value = stored_headers[i]['value']
 			for(j = 0; j< new_headers.length; j++){
 				if(_name == new_headers[j]['name'] && _value != new_headers[j]['value']){ //condition order optimized for performance
+					if(t == 1){console.log(_info, "For response headers")}
+					else if (t == 2){console.log(_info, "For request headers")}
 					console.log(_alert, _hdr_ch, "name: ", _name, "changed value: ", new_headers[j]['value'])
 				}
 			}
 		}
 	}
-	else console.log( _hdr_hp);
+	//else console.log( _hdr_hp);
 }
 
 
@@ -55,7 +64,7 @@ let headersReceived = function(){
 let completed = function(){
 	//console.log('completed', arguments)
 	if(headers_store[arguments[0].requestId]){
-		validateHeaders(headers_store[arguments[0].requestId], arguments[0].responseHeaders)
+		validateHeaders(headers_store[arguments[0].requestId], arguments[0].responseHeaders, 1)
 	}
 };
 
@@ -64,11 +73,15 @@ let completed = function(){
 let beforeSendHeaders = function(){
 	//console.log('beforeSendHeaders')
 	parameters_store[arguments[0].requestId] = arguments[0].url
+	req_headers[arguments[0].requestId] = arguments[0].requestHeaders
 };
 
 
 //Called when request is processed by extensions, then it checks the URL against stored URL
 let sendHeaders = function(){
+	if(req_headers[arguments[0].requestId]){
+		validateHeaders(req_headers[arguments[0].requestId], arguments[0].requestHeaders, 2)
+	}
 	//console.log('sendHeaders')
 	if(parameters_store[arguments[0].requestId] != arguments[0].url){
 		console.log(_alert, _prm_ch, "old: ", parameters_store[arguments[0].requestId], " modified: ", arguments[0].url)
@@ -91,12 +104,12 @@ let addExtPage = function(tabs){
 	ext_page_present = false
 	for(i = 0; i<= tabs.length; i++){
 		if(i == tabs.length){
-			chrome.tabs.create({url: 'chrome://extensions/'})
-			console.log("navigated chrome://extensions page")
+			chrome.tabs.create({url: _avoid_page})
+			console.log("navigated", _avoid_page ,"page")
 		}
 
-		else if(tabs[i].url == 'chrome://extensions/'){
-			console.log("Already there (chrome://extensions)")
+		else if(tabs[i].url.includes(_avoid_page)){
+			console.log("Already there (",_avoid_page,")")
 			break
 		}
 	}
@@ -104,7 +117,7 @@ let addExtPage = function(tabs){
 	//Now Adding listener to ext pages; whether any one is trying to close 'em.
 	chrome.tabs.getAllInWindow((newTabs) =>{
 		for(j = 0; j < newTabs.length; j++){
-			if(newTabs[j].url == 'chrome://extensions/'){
+			if(newTabs[j].url.includes(_avoid_page)){
 				ext_pages.push(newTabs[j].id)
 			}
 		}
@@ -122,7 +135,7 @@ var main = function(){
 	});
 
 
-	chrome.tabs.getAllInWindow(addExtPage);
+	setTimeout(3000, chrome.tabs.getAllInWindow(addExtPage));
 	chrome.webRequest.onHeadersReceived.addListener(headersReceived, {urls: ['<all_urls>']}, ['responseHeaders']);
 	chrome.webRequest.onCompleted.addListener(completed, {urls: ['<all_urls>']}, ['responseHeaders']);
 	chrome.webRequest.onBeforeSendHeaders.addListener(beforeSendHeaders, {urls: ['<all_urls>']});
@@ -132,7 +145,7 @@ var main = function(){
 	//Checking if any extension is avoiding chrome://extensions page
 	chrome.tabs.onRemoved.addListener((id) => {
 		if (ext_pages.includes(id)){
-			console.log(_alert, "chrome://extensions page being forcefully removed.")
+			console.log(_alert, _avoid_page," page being forcefully removed.")
 		}
 	});
 
@@ -140,7 +153,7 @@ var main = function(){
 	//not common | need to see this more | not clear
 	chrome.tabs.onReplaced.addListener((new_id, old_id) => {
 		if(ext_pages.includes(old_id)){
-			console.log(_alert, "chrome://extensions page being forcefully replaced.")
+			console.log(_alert, _avoid_page,"page being forcefully replaced.")
 		}
 	});
 
